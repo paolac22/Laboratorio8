6054574b9569b9d6f24ff7568232ccad9e5415fd
Paola <paoc.22@hotmail.com>
2022-10-31 22:36:22 -0500

    Segundo commit -Prueba

---
+++ src/test/java/Selenium_Pack/primera_prueba.java
@@ -0,0 +1,17 @@
+package Selenium_Pack;
+
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.chrome.ChromeDriver; 
+
+public class primera_prueba {
+	public static void main (String[] args)throws InterruptedException { 
+	 
+	 	System.setProperty("webdriver.chrome.driver","C:\\Users\\paoc2\\Downloads\\src\\test\\java\\chromedriver.exe"); 
+	 	 	WebDriver driver = new ChromeDriver();  	 	
+	 	 	driver.get("https://www.amazon.com/"); 
+	 	 	//inserta tu código aquí  	 	
+	 	 	Thread.sleep(5000); 
+	        driver.quit(); 
+	 	} 
+
+}
